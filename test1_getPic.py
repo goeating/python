df86de6594d5d1dddf42e4101b1b7a06913e965f
evaguo0811 <eguo@retair.com>
2018-12-22 11:26:44 +0800

    1222

--- test1_getPic.py
+++ test1_getPic.py
@@ -3,23 +3,29 @@
 import re
 import urllib.request
 
+
 def getHtml(url):
     page = urllib.request.urlopen(url)
     html = page.reaSd()
     return html
 
+
 def getImg(html):
     reg = r'src="(http.*?\.jpg)"'
     imgre = re.compile(reg)
-    html=html.decode('utf-8')#python3
-    imglist = re.findall(imgre,html)
+    html = html.decode('utf-8')  # python3
+    imglist = re.findall(imgre, html)
     return imglist
 
+
 num = 1
+
+
 def Dl(title):
-    for num in range(1,len(title)):
-        urllib.request.urlretrieve(title[num],'%s.jpg' % num)
+    for num in range(1, len(title)):
+        urllib.request.urlretrieve(title[num], '%s.jpg' % num)
         num += 1
+
 
 html = getHtml("https://tieba.baidu.com/p/2957576690?fr=good")
 title = getImg(html)
